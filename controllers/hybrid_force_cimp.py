some minor fixes in the jacobian computations

controllers/hybrid_force_cimp.py
@@ -27,7 +27,7 @@ def hybrid_force_cimp(model, data, X_d, V_d, K, A, f, stiffness_frame="reference
                                      K, A, and f are expressed
 
     Returns:
-        tau (np.ndarray) ... joint control torques
+        tau (np.ndarray) ... joint control torquesmjc_world_jacobian_derivative
         x_e (np.ndarray) ... pose error
         f_u  (np.ndarray) ... generated cartesian control force
 
@@ -45,6 +45,7 @@ def hybrid_force_cimp(model, data, X_d, V_d, K, A, f, stiffness_frame="reference
     X = sm.SE3.Rt(sm.base.smb.q2r(quat), data.site("panda_tool_center_point").xpos)
 
     J = mjc_body_jacobian(model, data)  # ee body jacobian expressed in the ee frame
+
     Jdot = mjc_body_jacobian_derivative(model, data)  # Jacobian derivative
     V = J @ dq  # current ee body twist
 
@@ -86,15 +87,15 @@ def hybrid_force_cimp(model, data, X_d, V_d, K, A, f, stiffness_frame="reference
     # task-space manipulator inertia
     qM = np.zeros((7, 7))
     mujoco.mj_fullM(model, qM, data.qM)
-    xM = np.linalg.pinv(J @ np.linalg.pinv(qM[0:7, 0:7]) @ J.transpose())
+    xM = np.linalg.inv(J @ np.linalg.inv(qM[0:7, 0:7]) @ J.transpose())
 
     # compute the control wrench, force control is feedforward only ([1], p. 441, eq. (11.61))
-    # f_u = P @ xM @ (B @ v_e + K @ x_e) + (np.eye(6) - P) @ f
-    f_u = xM @ S_M @ (E_B @ v_e + E_K @ x_e) + S_F @ E_f
+    # f_u = P @ xM @ (B @ v_e + K @ x_e) + S_F @ E_f
+    f_u = xM @ (B @ v_e + K @ x_e)
 
     # Mapping the external control wrench to joint torques and compensating
     # the manipulator dynamics (gravity + coriolis / centripetal only).
-    tau = data.qfrc_bias[0:7] + J.transpose() @ (f_u - xM @ Jdot @ data.qvel[0:7])
+    tau = data.qfrc_bias[0:7] + J.transpose() @ (f_u - xM @ Jdot @ dq)
 
     # add the nullspace torques which will bias the manipulator to the desired
     # nullspace bias configuration
@@ -107,4 +108,4 @@ def hybrid_force_cimp(model, data, X_d, V_d, K, A, f, stiffness_frame="reference
     tau_ns = qM[0:7, 0:7] @ ddq_ns
 
     # return x_e and f_u for introspection, in addition to the control torques
-    return tau + tau_ns, x_e, f_u
+    return tau + tau_ns * 0, x_e, f_u

hybrid_motion_force_control.py
@@ -20,35 +20,45 @@ def simulate(
     x = np.zeros((t_vec.size, 3))
     x_d = np.zeros((t_vec.size, 3))
     # launch MuJoCo viewer
-    with mujoco.viewer.launch_passive(model, data) as viewer:
-        # Close the viewer automatically after the simulation duration expires
-        for i, t in enumerate(t_vec):
-            step_start = time.time()
-            x[i, :] = data.site("panda_tool_center_point").xpos
-            x_d[i, :] = X_d[i].t
+    # with mujoco.viewer.launch_passive(model, data) as viewer:
+    # Close the viewer automatically after the simulation duration expires
+    for i, t in enumerate(t_vec):
+        step_start = time.time()
+        x[i, :] = data.site("panda_tool_center_point").xpos
+        x_d[i, :] = X_d[i].t
 
-            # advances simulation by all non-control dependent quantities
-            mujoco.mj_step1(model, data)
+        # advances simulation by all non-control dependent quantities
+        mujoco.mj_step1(model, data)
 
-            # evaluate the controller to get the control torques, as well as pose error
-            # and control wrenches for introspection
-            tau, x_e[i, :], f_e[i, :] = hybrid_force_cimp(
-                model, data, X_d[i], V_d[i], K, A, f, stiffness_frame
-            )
+        # evaluate the controller to get the control torques, as well as pose error
+        # and control wrenches for introspection
+        tau, x_e[i, :], f_e[i, :] = hybrid_force_cimp(
+            model, data, X_d[i], V_d[i], K, A, f, stiffness_frame
+        )
 
-            # set the MuJoCo controls according to the computed torques
-            data.ctrl[0:7] = tau
+        # set the MuJoCo controls according to the computed torques
+        data.ctrl[0:7] = tau
 
-            # advance simulation fully, including the control torques
-            mujoco.mj_step2(model, data)
+        # advance simulation fully, including the control torques
+        mujoco.mj_step2(model, data)
 
-            # update the viewer
-            viewer.sync()
+        # update the viewer
+        # viewer.sync()
 
-            # Rudimentary real-time keeping for visualization
-            time_until_next_step = model.opt.timestep - (time.time() - step_start)
-            if time_until_next_step > 0:
-                time.sleep(time_until_next_step)
+        from simulation.mujoco_helpers import (
+            mjc_world_jacobian,
+            mjc_world_jacobian_derivative,
+        )
+
+        J = mjc_world_jacobian(
+            model, data
+        )  # ee body jacobian expressed in the ee frame
+        Jdot = mjc_world_jacobian_derivative(model, data)
+
+        # Rudimentary real-time keeping for visualization
+        time_until_next_step = model.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
 
     # Plotting
     if plotting:
@@ -99,14 +109,14 @@ if __name__ == "__main__":
     K = np.diag(np.hstack((np.ones(3) * k_t, np.ones(3) * k_r)))
 
     # Specify a desired contact wrench
-    f = np.array([0, 0, 10.0, 0, 0, 0])
+    f = np.array([0, 0, 0.0, 0, 0, 0])
 
     # Specify a desired Pfaffian constraint matrix A (see Lynch textbook (https://hades.mech.northwestern.edu/images/7/7f/MR.pdf), pp. 439)
     # This is a k x 6 matrix, where k is the number of end-effector twist constraints, i.e., A * V = 0. In the context of hybrid
     # force/motion control, this means that the end-effector is free to move in 6-k directions, and constrained (i.e., force-controlled) in k directions.
 
     A = np.zeros((6, 6))
-    A[2, 2] = 1
+    # A[2, 2] = 1
 
     # control & simulation timestep
     timestep = 0.005
@@ -134,7 +144,7 @@ if __name__ == "__main__":
     model.opt.timestep = timestep
 
     # compute forward dynamcis to update kinematic quantities
-    mujoco.mj_forward(model, data)
+    mujoco.mj_fwdPosition(model, data)
 
     # current ee pose- xM @ Jdot @ data.qvel[0:7]
     X_0 = sm.SE3.Rt(
@@ -146,9 +156,10 @@ if __name__ == "__main__":
     # a test reference trajectory consisting simply of a constant setpoint with zero desired twist
     # X_d = [X_0] * int(duration / timestep)
     # V_d = [sm.Twist3()] * int(duration / timestep)
+    # dV_d = [sm.Twist3()] * int(duration / timestep)
 
     # a test reference trajectory describing a back-and-forth motion along one axis
-    X_d, V_d = generate_line_motion(X_0, timestep, duration, 20)
+    X_d, V_d = generate_line_motion(X_0, timestep, duration, 1)
 
     # run control & sim loop
     simulate(model, data, X_d, V_d, K, A, f, stiffness_frame, plotting)

simulation/mujoco_helpers.py
@@ -33,8 +33,32 @@ def mjc_body_jacobian(model, data):
     return np.vstack((R_ew @ jacp[:, 0:7], R_ew @ jacr[:, 0:7]))
 
 
+def mjc_world_jacobian(model, data):
+    """
+    Compute the body jacobian which relates joint velocities to the body twist (i.e.,
+    the end-effector linear, angular velocity expressed in the world frame).
+    Currently, this method is hardcoded for the model of the Franka Emika Panda robot.
+    It is assumed, that the kinematic quantities in 'data' are up-to date, i.e., that
+    at least mj_forward(.) was called prior to this function.
+
+    Args:
+        model ... MuJoCo model struct
+        data ... Mujoco data struct
+
+    returns
+        jac ... body jacobian
+    """
+    jacp = np.zeros((3, model.nv), dtype=np.float64)
+    jacr = np.zeros((3, model.nv), dtype=np.float64)
+
+    # compute position/rotation parts of the jacobian
+    mujoco.mj_jacSite(model, data, jacp, jacr, model.site("panda_tool_center_point").id)
+
+    return np.vstack((jacp[:, 0:7], jacr[:, 0:7]))
+
+
 def mjc_body_jacobian_derivative(model, data):
-    '''
+    """
     Computes the body jacobian derivative via forward simulation by a small timestep
     and numerical differentiation as suggested here: https://github.com/google-deepmind/mujoco/issues/411.
     Currently, this method is hardcoded for the model of the Franka Emika Panda robot.
@@ -47,14 +71,43 @@ def mjc_body_jacobian_derivative(model, data):
 
     returns
         jac_dot ... time derivative of the body jacobian
-    '''
+    """
 
     h = 1e-10
     qpos = deepcopy(data.qpos)
     J = mjc_body_jacobian(model, data)
     mujoco.mj_integratePos(model, data.qpos, data.qvel, h)
-    mujoco.mj_forward(model, data)
+    mujoco.mj_fwdPosition(model, data)
     Jh = mjc_body_jacobian(model, data)
+    Jdot = (Jh - J) / h
+
+    # qpos needs to be reset in the data struct
+    data.qpos = qpos
+    return Jdot
+
+
+def mjc_world_jacobian_derivative(model, data):
+    """
+    Computes the body jacobian derivative (expressed in the world frame) via forward simulation by a small timestep
+    and numerical differentiation as suggested here: https://github.com/google-deepmind/mujoco/issues/411.
+    Currently, this method is hardcoded for the model of the Franka Emika Panda robot.
+    It is assumed, that the kinematic quantities in 'data' are up-to date, i.e., that
+    at least mj_forward(.) was called prior to this function.
+
+    Args:
+        model ... MuJoCo model struct
+        data ... Mujoco data struct
+
+    returns
+        jac_dot ... time derivative of the body jacobian
+    """
+
+    h = 1e-10
+    qpos = deepcopy(data.qpos)
+    J = mjc_world_jacobian(model, data)
+    mujoco.mj_integratePos(model, data.qpos, data.qvel, h)
+    mujoco.mj_fwdPosition(model, data)
+    Jh = mjc_world_jacobian(model, data)
     Jdot = (Jh - J) / h
 
     # qpos needs to be reset in the data struct